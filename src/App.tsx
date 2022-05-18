eaf3a585e5e7f58e080e74506dd1ae95bf633e1e
alex_mosiakin <mosyakin.a@flat12.ru>
2022-05-18 14:59:54 +0300

    Create basic chess without king's moves, lose conditions, steps history and end game by the timer

---
+++ src/App.tsx
@@ -0,0 +1,59 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+import BoardComponent from './components/BoardComponent';
+import { Board } from './models/Board';
+import { Colors } from './models/Colors';
+import { Player } from './models/Player';
+
+import LostFigures from './components/LostFigures';
+import Timer from './components/Timer';
+
+function App() {
+  const [board, setBoard] = useState(new Board())
+  const [whitePlayer, setWhitePlayer] = useState(new Player(Colors.WHITE))
+  const [blackPlayer, setBlackPlayer] = useState(new Player(Colors.BLACK))
+  const [currentPlayer, setCurrentPlayer] = useState<Player | null>(null)
+
+  function restart(){
+    const newBoard = new Board();
+    newBoard.initCells();
+    newBoard.addFigures();
+    setBoard(newBoard);
+  }
+
+  useEffect(() => {
+    restart();
+    setCurrentPlayer(whitePlayer)
+  },[])
+
+  function swapPlayer(){
+    setCurrentPlayer(currentPlayer?.color === Colors.WHITE ? blackPlayer : whitePlayer)
+  }
+
+  return (
+    <div className="app">
+      <Timer 
+        restart={restart}
+        currentPlayer={currentPlayer}
+      />
+      <BoardComponent 
+        board={board} 
+        setBoard={setBoard}
+        currentPlayer={currentPlayer}
+        swapPlayer={swapPlayer}
+      />
+      <div>
+        <LostFigures 
+          title="Black"
+          figures={board.lostBlackFigures}
+        />
+        <LostFigures 
+          title="White"
+          figures={board.lostWhiteFigures}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
